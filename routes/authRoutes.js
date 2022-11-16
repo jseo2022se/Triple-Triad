52397021cb8e79297b6a264d75b4f9bee44e230a
Jeonghyun Seo <jseo2022se@gmail.com>
2022-11-16 14:56:05 -0500

    implementing test route for deployment

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,5 +5,6 @@ const authCtrl = require('../controllers/authController')
 
 router.post('/register', authCtrl.register)
 router.post('/login', authCtrl.login)
+router.get('/testroute', authCtrl.testRoute)
 
 module.exports = router
